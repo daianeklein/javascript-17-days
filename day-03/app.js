0ec08759dcfb56b6db9671652566596fcb077803
daianeklein <daiane.klein22@gmail.com>
2023-06-07 08:25:12 -0300

    add day03

---
+++ day-03/app.js
@@ -0,0 +1,11 @@
+//incremento
+let numProdutos = 2;
+let numProdutos2 = 2;
+
+let totalProdutos2 = ++numProdutos;
+let totalProdutos = numProdutos++;
+console.log(numProdutos); //2
+console.log(totalProdutos); //3
+
+console.log(numProdutos2); //3
+console.log(totalProdutos2); //3
---
+++ day-03/notas/app.js
@@ -0,0 +1,7 @@
+let notaBimestre1 = 7;
+let notaBimestre2 = 8;
+let notaBimestre3 = 9;
+let notaBimestre4 = 9;
+
+let mediaNotas = (notaBimestre1 + notaBimestre2 + notaBimestre3 + notaBimestre4) / 4;
+console.log(mediaNotas)
