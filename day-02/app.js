49ba1cd1c1857fba03bec6b337b2f936d430bb53
daianekoya <daiane@getkoya.ia>
2023-06-07 07:33:42 -0300

    add day02

---
+++ day-02/app.js
@@ -0,0 +1,15 @@
+//let meuNome;
+//console.log(meuNome);
+
+let nome = "Manuel";
+let sobrenome = "Silva";
+let idade = 24;
+const numCliente = 12345;
+
+console.log(nome);
+console.log(sobrenome);
+console.log(idade);
+console.log(numCliente);
+
+
+
